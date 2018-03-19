fe38423ff4ca71c626b8ec4cd50f03f255a8a203
Yoann <yoann.cochennec@infotel.com>
2018-03-19 15:10:02 +0100

    ajout des modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { BookComponent } from './book/book.component';
+import { MemberComponent } from './member/member.component';
+import { ResearchComponent } from './research/research.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    RegistrationComponent,
+    BookComponent,
+    MemberComponent,
+    ResearchComponent
   ],
   imports: [
     BrowserModule
